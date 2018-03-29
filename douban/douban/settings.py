5b6d6564a73b176f7170facec5ed45653c0b8186
Bushiczl <32673543+Bushiczl@users.noreply.github.com>
2018-03-29 15:21:38 +0800

    trying

--- douban/douban/settings.py
+++ douban/douban/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'book',
+    'users',
+    'main',
 ]
 
 MIDDLEWARE = [
--- douban/main/admin.py
+++ douban/main/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from .models import Article
 
+
 class ArticleAdmin(admin.ModelAdmin):
 	"""docstring for A"""
 	list_display=('title','pub_date','update_time',)
 
-	
-		
 
 # Register your models here.
-admin.site.register(Article,ArticleAdmin)
+admin.site.register(Article, ArticleAdmin)

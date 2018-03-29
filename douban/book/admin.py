59bc9322027896080115b3ad664695cf1984adfd
Bushiczl <32673543+Bushiczl@users.noreply.github.com>
2018-03-29 14:14:28 +0800

    admin

--- douban/book/admin.py
+++ douban/book/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Book, Comment, Label
 
+
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('bookname', 'author', 'label')
+    fieldsets = (
+        [u'主要信息', {
+            'fields': ('bookname', 'author', 'introduction'),
+        }],
+        [u'附加', {
+            'fields': ('label', 'cover'),
+        }]
+    )
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Label)
 admin.site.register(Comment)

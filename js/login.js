018abfd6ebc740fcd5a1d5c40187ee41af74531f
yuyuan7 <yuyuan7@126.com>
2018-04-01 08:24:45 +0800

    code_forget ok error ok

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 //验证码切换
 $(".login_form_verify_image").click(function(){
 
-	$(this).css("background-image",'url("../images/book_list_logo.png")');
+	$(".login_form_verify_image_img").attr('src','../images/book_list_logo.png');
 })
 
 //logo 变化
@@ -24,7 +24,7 @@ $(".login_form_name_input").blur(function(){
    var content=$(this).val();
   	if(content==""){
   		$(this).css({'color':'#C9C9C9'});
-  		$(this).attr("value","请输入正确的邮箱地址");
+  		$(this).attr("value","example@xxx.com");
 	}
 	else{
 		$(this).css({'color':'black'});
@@ -37,6 +37,8 @@ $(".login_form_name_input").blur(function(){
 var name_wrong = false;
 var code_wrong = false;
 var jump = true;
+
+
 if(name_wrong == true)
 {
   $(".login_form_name_tip").show();
@@ -62,3 +64,18 @@ $(".login_form_verify_button").click(function(){
 }
 
 });
+
+
+function validate_email(field,alerttxt)
+{
+with (field)
+{
+apos=value.indexOf("@")
+dotpos=value.lastIndexOf(".")
+if (apos<1||dotpos-apos<2) 
+
+  { name_wrong = false;return false}
+else {return true}
+}
+}
+

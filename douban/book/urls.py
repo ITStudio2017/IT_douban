fbc58b3fcda20808d8f22a41ffc3fc0e6ae41d0b
Alex <alex-zs@foxmail.com>
2018-04-15 10:22:53 +0800

    bug修复

--- douban/book/urls.py
+++ douban/book/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^uncollect/(\d+)/$', book_uncollect, name='book_uncollect'),
     url(r'^search/(.*)/$',search,name='search'),
     url(r'^jump/$',jump,name='jump'),
-    url(r'^commentdelete/(?P<id>[0-9]+)$',comment_delete,name='comment_delete'),
+    url(r'^commentdelete/(?P<id>[0-9]+)/$',comment_delete,name='comment_delete'),
     # url(r'^get_label/(?P<obj_id>\d+)', twice_label_choice),
 ]
